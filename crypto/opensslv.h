f9059220232320f8e5edeb9f1400f7c34e59e381
Dirk <dirk@testssl.sh>
2022-09-01 16:10:45 +0200

    Renaming to the fork to -bad

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -32,9 +32,9 @@ extern "C" {
  */
 # define OPENSSL_VERSION_NUMBER  0x100020b0L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-chacha (1.0.2k-fips-dev)"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-bad (1.0.2k-fips-dev)"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-chacha (1.0.2k-dev)"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-bad (1.0.2k-dev)"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
